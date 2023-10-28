5d34e90bbf674446ba309b6c96d49b403d020df2
Renato Franco <renato.franco@avantsoft.com.br>
2023-10-27 21:41:42 -0300

    Initial Commit

---
+++ Landemarks/LandemarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandemarksApp.swift
+//  Landemarks
+//
+//  Created by Renato Franco on 27/10/23.
+//
+
+import SwiftUI
+
+@main
+struct LandemarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
