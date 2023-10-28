c230af4729bc4c1845479b058ecbade18c9f4bff
Renato Rosa Franco <renato_ny@live.com>
2023-10-28 19:00:22 -0300

    feature: Working with UI controls.

---
+++ Landemarks/Views/Profile.swift
@@ -0,0 +1,26 @@
+//
+//  Profile.swift
+//  Landemarks
+//
+//  Created by Renato Franco on 28/10/23.
+//
+
+import Foundation
+
+struct Profile {
+    var username: String
+    var preferNotifications = true
+    var seasonalPhoto = Season.winter
+    var goalDate = Date()
+    
+    static let `default` = Profile(username: "g_kumar")
+    
+    enum Season: String, CaseIterable, Identifiable {
+        case spring = "🌷"
+        case summer = "🌞"
+        case autumn = "🍂"
+        case winter = "☃️"
+
+        var id: String { rawValue }
+    }
+}
